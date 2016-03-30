87744d6a620db60d7a5f7eadd9fec579633a2208
Guillaume <guillaume.kheng@insa-lyon.fr>
2016-03-30 10:05:04 +0200

    Added UNIX GRILS v1.0

---
+++ Grills_UNIX/sources/Config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Change TEST value to 0 to deactivate testing procedures
+
+#define TEST false
+
+#define TRACE true
+
+
+// speed of each visitor.
+const float f_VISITOR_SPEED = 1;
+
+#endif //  CONFIG_H
---
+++ Grills_UNIX/sources/types.h
@@ -0,0 +1,62 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <vector>
+
+using namespace std;
+
+
+// CONSTANTS
+
+
+
+
+// STRUCTS
+
+typedef struct { // Time Window (TW)
+
+	float O; // openning hour
+	float C; // closing hour
+
+} TW;
+
+
+struct Vertex { // Vertex
+
+	// Should you change anything here don't forget to implement accordingly copyVertex(p_vtx) described in function.h
+
+	int id; //------------ unique id
+	float x; //----------- x coordinate
+	float y; //----------- y coordinate
+	float score; //------- score associated with the vertex
+
+	// Constraints
+	vector<char> v_c_ICN; //--- v_c_ICN[w] = [I if Included / C if Considered / N if Not possible to include] in turn w
+	float v_constraint[10]; //- Slack consumption of the vertex
+	float fee; //-------------- fee related to the vertex 
+
+	// Time handling
+	vector<TW *> v_tws; // Time Windows of the vertex (in chronological order)
+	float serviceTime; //- time necessary to complete the visit
+	float TA; //---------- time of arrival
+	float Departure; //--- time of departure
+	float Wait;	//-------- waiting time
+	float MaxShift; //---- Maximum shifting time possibility
+	int e; //------------- Exchange of Time Windows (cf format Description)
+
+	// Algorithm optimisation
+	bool isInserted; //----- Indicates if turn is inseted in a turn or not.
+	vector<struct Vertex *> v_p_vtx_Family;
+
+};typedef struct Vertex Vertex;
+
+typedef struct { // Contains the informations related to the insertion of the vertex into the turn.
+
+	int index; //-------------- Index of insertion
+	float heuristicScore; //--- EuristicScore of the insertion
+	Vertex * p_vtx; //--------- Vertex concerned
+	int n_turn; //------------- [Turn] concerned / cast necessary because of compilation
+
+} Insertion;
+
+#endif
